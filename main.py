e61d3b9f66cc7ea5ebae0f40081a468c37b53da8
vedgie <deanvettier@gmail.com>
2024-01-31 00:01:33 +1100

    Code updated again because I'm bad

--- main.py
+++ main.py
@@ -9,30 +9,34 @@ load_dotenv()
 
 intents = nextcord.Intents.default()
 intents.messages = True
+intents.guilds = True
+intents.message_content = True
 bot = commands.Bot(intents=intents)
 
-image_counter = {}
+image_counter = 0
+post_amount = {}
 
 def contains_url(text):
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     return bool(url_pattern.search(text))
 
+post_amount = random.randint(15, 20)
+print (f"Post Amount: {post_amount}")
+
 @bot.event
 async def on_message(message):
         if message.attachments or message.embeds or contains_url(message.content):
-            channel_id = message.channel.id
+            global image_counter
+            global post_amount
+            image_counter += 1
+            print(f"Image counter: {image_counter}")
 
-            if channel_id not in image_counter:
-                 image_counter[channel_id] = 0
-
-            image_counter[channel_id] += 1
-            print(f"Image counter for channel {channel_id}: {image_counter[channel_id]}")
-
-            if image_counter[channel_id] == random.randint(20, 30):
-                image_counter[channel_id] = 0
+            if image_counter == post_amount:
+                image_counter = 0
+                post_amount = random.randint(15, 20)
+                print (f"Post Amount: {post_amount}")
                 await message.channel.send("jfc bro")
 
-        
         await bot.process_commands(message)
 
 bot.run(os.getenv('BOT_TOKEN'))
